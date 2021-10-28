6a58c0d8371deef871505de1d58a2420cbc81dc5
Tobias Egelund <egelundtobias@gmail.com>
2021-10-28 07:34:25 +0200

    brew script

---
+++ bash/brew.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+
+# SETUP
+which -s brew
+if [[ $? != 0 ]] ; then
+    echo "Installing Homebrew"
+    /usr/bin/bash -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+echo "Upgrading Homebrew"
+brew upgrade
+echo "Updating Homebrew"
+brew update
+echo "Installing brew cask"
+brew tap caskroom/cask
+echo # newline
+
+
+# INSTALL BREW APPS
+declare -a apps=(
+    'git'
+		'htop'
+    'node'
+    'redis'
+    'yarn'
+)
+echo "Installing brew apps"
+for app in "${apps[@]}"; do
+    echo "      Installing ${app}"
+    brew install "$app"
+done
+echo # newline
+
+
+# INSTALL BREW CASK APPS
+declare -a cask_apps=(
+    'discord'
+    'docker'
+    'firefox'
+    'google-chrome'
+    'keepingyouawake'
+    'postgresql'
+    'slack'
+    'spotify'
+    'visual-studio-code'
+    'zoom'
+)
+echo "Installing brew cask apps"
+for cask_app in "${cask_apps[@]}"; do
+    echo "      Installing ${cask_app}"
+    brew install --cask "$cask_app"
+done
+echo # newline
+
+
+# CLEANUP
+brew cleanup
